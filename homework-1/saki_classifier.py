b3ca7279ad0d5a713e45b4c6be61cd57620a493b
Marlon Weghorn <mw2015@posteo.de>
2021-05-28 16:56:29 +0200

    homework-2

--- homework-1/saki_classifier.py
+++ homework-1/saki_classifier.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
@@ -18,6 +18,9 @@ if __name__ == '__main__':
 
     df['Buchungstext'] = df['Buchungstext'].str.replace(r'\W', ' ', regex=True)
     df['Buchungstext'] = df['Buchungstext'].str.lower()
+    #currency = df['Verwendungszweck'].\
+    #    str.findall(r'\d+[.,,]\d+').apply(lambda s: ' '.join(map(lambda f: format(float(f.replace(',', '.')), '.2f'), s)))
+    #df['Betrag'] = df['Betrag'].apply(lambda f: format(float(f.replace('-', '').replace(',', '.')), '.2f'))
     df['Verwendungszweck'] = df['Verwendungszweck'].str.replace(r'\W', ' ', regex=True)
     df['Verwendungszweck'] = df['Verwendungszweck'].str.replace(r'\d', '', regex=True)
     df['Verwendungszweck'] = df['Verwendungszweck'].str.replace(r'\b\w{1,2}\b', '', regex=True)
@@ -44,24 +47,34 @@ if __name__ == '__main__':
     # 4. transform features into a usable format and 5. train model                 #
     #################################################################################
 
-    X_train, X_test, y_train, y_test = train_test_split(data['message'], df['label'])
+    num_runs = 1
+    accuracy_sum = 0
 
-    text_clf = Pipeline([
-        ('vect', CountVectorizer()),
-        ('clf', MultinomialNB()),
-    ])
-    parameters = {
-        'vect__ngram_range': [(1, 1), (1, 2), (2, 2)],
-    }
-    gs_clf = GridSearchCV(text_clf, parameters, cv=5, n_jobs=-1)
-    gs_clf.fit(X_train, y_train)
+    for _ in range(num_runs):
+        X_train, X_test, y_train, y_test = train_test_split(data['message'], df['label'], random_state=42)
 
-    #################################################################################
-    # 6. evaluate model                                                             #
-    #################################################################################
+        text_clf = Pipeline([
+            ('vect', CountVectorizer()),
+            ('clf', MultinomialNB()),
+        ])
+        parameters = {
+            'vect__ngram_range': [(1, 1), (1, 2), (2, 2)],
+        }
+        gs_clf = GridSearchCV(text_clf, parameters, cv=5, n_jobs=-1)
+        gs_clf.fit(X_train, y_train)
 
-    predictions = gs_clf.predict(X_test)
-    print(classification_report(y_test, predictions, target_names=le.classes_))
+        #################################################################################
+        # 6. evaluate model                                                             #
+        #################################################################################
+
+        predictions = gs_clf.predict(X_test)
+
+        report_dict = classification_report(y_test, predictions, target_names=le.classes_)
+
+        accuracy_sum += accuracy_score(y_test, predictions)
+
+    print(report_dict)
+    print(f"Overall accuracy of {num_runs} runs: " + str(accuracy_sum / num_runs))
 
     for param_name in sorted(parameters.keys()):
         print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
